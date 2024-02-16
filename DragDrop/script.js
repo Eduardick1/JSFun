ed0ae148b3ae5f205aad42bba3d71996e353bf57
Eduard <ed.sushh@gmail.com>
2024-02-16 16:10:06 +0300

    update

--- DragDrop/script.js
+++ DragDrop/script.js
@@ -1,32 +1,138 @@
-const drag = document.querySelector('.drag')
-const places = document.querySelectorAll('.place')
+const drag = document.querySelector(".drag");
+const places = document.querySelectorAll(".place");
 
-drag.addEventListener('dragstart', (e) => {
-	e.target.classList.add('hold');
-	setTimeout(() => e.target.classList.add('hide'), 0)
-})
-drag.addEventListener('dragend', (e) => {
-	e.target.className = 'drag'
-})
+drag.addEventListener("dragstart", (e) => {
+  e.target.classList.add("hold");
+  setTimeout(() => e.target.classList.add("hide"), 0);
+});
+drag.addEventListener("dragend", (e) => {
+  e.target.className = "drag";
+});
 
 function dragOver(e) {
-	e.preventDefault()
+  e.preventDefault();
 }
 function dragEnter(e) {
-	e.target.classList.add('hovered')
+  e.target.classList.add("hovered");
 }
 function dragLeave(e) {
-	e.target.classList.remove('hovered')
+  e.target.classList.remove("hovered");
 }
 function dragDrop(e) {
-	e.target.append(drag)
-	e.target.classList.remove('hovered')
+  e.target.append(drag);
+  e.target.classList.remove("hovered");
 }
 
-
 places.forEach((p) => {
-p.addEventListener('dragover', dragOver)
-p.addEventListener('dragenter', dragEnter)
-p.addEventListener('dragleave', dragLeave)
-p.addEventListener('drop', dragDrop)
-})
+  p.addEventListener("dragover", dragOver);
+  p.addEventListener("dragenter", dragEnter);
+  p.addEventListener("dragleave", dragLeave);
+  p.addEventListener("drop", dragDrop);
+});
+
+// console.log(int);
+
+// let int;
+
+// console.log(int);
+
+// int = 10;
+
+// console.log(int);
+
+// let T = null;
+
+// console.log((T ??= false));
+// console.log((T ??= 20));
+// console.log((T ??= undefined));
+
+// напиши переменную _____ в pascal case, snake case, flat case, kebab case, camel case
+
+// const s = {
+//   r: 10,
+//   d() {
+//     return this.r * 2;
+//   },
+//   p: () => 2 * this.r,
+// };
+// console.log(s.d());
+// console.log(s.p());
+
+// /
+
+// console.log(a, b, c, d);
+// function cs(a, b) {
+//   a *= 10;
+//   b.item = "changed";
+// }
+
+// let n = 10;
+// let obj = {
+//   item: "inchanged",
+// };
+// cs(n, obj);
+
+// console.log(n, obj);
+
+// const info = {
+//   text: "desc",
+// };
+// info[info.text] = " some text";
+
+// console.log(info);
+
+// const a = () => {
+//   console.log("ani");
+// };
+
+// requestAnimationFrame(a);
+// class Logger {
+//   log(text) {
+//     console.log(text);
+//   }
+// }
+
+// class Printer extends Logger {
+//   constructor(name) {
+//     super();
+//     this.name = name;
+//   }
+//   log(text) {
+//     super.log(`${this.name}: ${text}`);
+//   }
+// }
+
+// const p = new Printer("only");
+
+// p.log("hello");
+
+// let i = 5;
+// while (i) {
+//   console.log(i--);
+// }
+
+// try {
+//   const x = 123;
+//   x++;
+// } catch (e) {
+//   console.log("unexp err");
+// } finally {
+//   console.log("finally", x);
+// }
+let ArrayofArrays = [
+  [0, 1, 1, 1, 2, 3, 3, 3, 6, 7, 8],
+  [1, 1, 2],
+  [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6],
+];
+
+function removeDuplicates(nums) {
+  const removed = nums.reduce((acc, n, i) => {
+    if (nums[i + 1] != n) acc.push(n);
+    return acc;
+  }, []);
+  console.log([removed.length, removed]);
+}
+
+for (let arr of ArrayofArrays) {
+  removeDuplicates(arr);
+}
---
+++ TreeFolders/script.js
@@ -0,0 +1,140 @@
+const DATA = {
+  services: [
+    {
+      id: 1,
+      head: null,
+      name: "Проф.осмотр",
+      node: 0,
+      price: 100.0,
+      sorthead: 20,
+    },
+    {
+      id: 2,
+      head: null,
+      name: "Хирургия",
+      node: 1,
+      price: 0.0,
+      sorthead: 10,
+    },
+    {
+      id: 3,
+      head: 2,
+      name: "Удаление зубов",
+      node: 1,
+      price: 0.0,
+      sorthead: 10,
+    },
+    {
+      id: 4,
+      head: 3,
+      name: "Удаление зуба",
+      node: 0,
+      price: 800.0,
+      sorthead: 10,
+    },
+    {
+      id: 5,
+      head: 3,
+      name: "Удаление 8ого зуба",
+      node: 0,
+      price: 1000.0,
+      sorthead: 30,
+    },
+    {
+      id: 6,
+      head: 3,
+      name: "Удаление осколка зуба",
+      node: 0,
+      price: 2000.0,
+      sorthead: 20,
+    },
+    {
+      id: 7,
+      head: 2,
+      name: "Хирургические вмешательство",
+      node: 0,
+      price: 200.0,
+      sorthead: 10,
+    },
+    {
+      id: 8,
+      head: 2,
+      name: "Имплантация зубов",
+      node: 1,
+      price: 0.0,
+      sorthead: 20,
+    },
+    {
+      id: 9,
+      head: 8,
+      name: "Коронка",
+      node: 0,
+      price: 3000.0,
+      sorthead: 10,
+    },
+    {
+      id: 10,
+      head: 8,
+      name: "Слепок челюсти",
+      node: 0,
+      price: 500.0,
+      sorthead: 20,
+    },
+  ],
+};
+
+// рекурсивная функция принимающая: массив даннных, id родителя списка и самого родителя
+function buildTree(data, parentId, container) {
+  const ul = document.createElement("ul");
+  data.forEach((item) => {
+    if (item.head === parentId) {
+      const li = document.createElement("li");
+      switch (item.node) {
+        case 0: {
+          li.classList.add("element");
+          li.innerHTML = `<span>${item.name}, ${item.price} руб.</span>`;
+          break;
+        }
+        case 1: {
+          li.classList.add("node");
+          li.innerHTML = `<span>${item.name}</span>`;
+          break;
+        }
+      }
+      if (item.node === 0) {
+        li.classList.add("element");
+        li.innerHTML = `<span>${item.name}, ${item.price} руб.</span>`;
+      } else if (item.node === 1) {
+        li.classList.add("node");
+        li.innerHTML = `<span>${item.name}</span>`;
+      }
+
+      ul.appendChild(li);
+      const children = buildTree(data, item.id, ul);
+      if (children) {
+        li.appendChild(children);
+      }
+    }
+  });
+  return ul.childNodes.length ? ul : null;
+}
+
+// Сортировка данных по sorthead
+const sortedData = DATA.services.sort((a, b) => a.sorthead - b.sorthead);
+
+// Построение и отображение дерева
+const treeContainer = document.querySelector(".container");
+const tree = buildTree(sortedData, null, treeContainer);
+if (tree) {
+  treeContainer.appendChild(tree);
+}
+
+treeContainer.onclick = (e) => {
+  console.log(e);
+  if (e.target.tagName != "SPAN") return;
+
+  const children = e.target.parentNode.querySelector("ul");
+  if (!children) return;
+  children.parentNode.classList.toggle("closed");
+  children.hidden = !children.hidden;
+};
